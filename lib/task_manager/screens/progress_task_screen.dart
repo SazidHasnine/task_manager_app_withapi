5f44fe59af4af1e29d726ee4cf83a2027f595ed1
Your Name <sazidhas9steam@gmail.com>
2026-08-06 12:41:43 +0600

    UI built

---
+++ lib/task_manager/screens/progress_task_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProgessTaskScreen extends StatefulWidget {
+  const ProgessTaskScreen({super.key});
+
+  @override
+  State<ProgessTaskScreen> createState() => _ProgessTaskScreenState();
+}
+
+class _ProgessTaskScreenState extends State<ProgessTaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
